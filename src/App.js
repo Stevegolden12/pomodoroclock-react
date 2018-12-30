8fc7d7916d490f1a70ac7a78d576e5e5d753ef3c
Steve Golden <sgolden2525@outlook.com>
2018-12-30 01:29:39 -0600

    Added planned structure
    Added basic structure
    Planned the rest of the tasks through notes throughout the code
    Removed the Prototype duplicate.

--- src/App.js
+++ src/App.js
@@ -6,55 +6,93 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      breakVal: 5,
+      sessionVal: 25,
+    }
+  }
   render() {
     return (
       <div className="App">
-        <LabelComp idName="break-label" />
-        <LabelComp idName="session-label" />
-        <LengthComp idName="break-length" />
-        <LengthComp idName="session-length" />
+        {/*                                 Add Container/Grid CSS layout                                       */}
+        <div>Pomodoro Clock</div>
+        <DecreaseClick />
+        <LabelComp idName="break-label" labelName="Break Label" />
+        <IncreaseClick />
+        <DecreaseClick />
+        <LabelComp idName="session-label" labelName="Session Label" />
+        <IncreaseClick />
+        <label idName="break-length">{this.state.breakVal}</label>
+        <label idName="session-length">{this.state.sessionVal}</label>
+        <Timer />
       </div>
     );
   }
 }
 
+
 const LabelComp = (props) => {
   return (
-    <div id={props.idName}></div>     
+    <div id={props.idName}>{props.labelName}</div>     
   )
 }
-/*  break is 5 and session is 25 must add a props*/
-const LengthComp = (props) => {
-  return (
-    <div id={props.idName}></div>  
-  )
-}
+/*                               break is 5 and session is 25 must add a props                                                  */
 
 class IncreaseClick extends React.Component {
 
-/* Need to add a increase method AND make sure it can work with 
- * break and session*/
   constructor(props) {
     super(props);
+
   }
+                             //Add the methods breakIncrease and sessionIncrease
+
+
   render() {
     return (
+                      //Add the code that whatever session/break props is passed you use that method to adjust the inputs
       <div id={this.props.idName}></div>  
     )
   }
 }
 
 class DecreaseClick extends React.Component {
+    //Add the methods breakDecrease and sessionDecrease
 
-  /* Need to add a decrease method AND make sure it can work with 
-   * break and session*/
-  constructor(props) {
+
+ constructor(props) {
     super(props);
   }
   render() {
     return (
+    //Add the code that whatever session/break props is passed you use that method to adjust the inputs
       <div id={this.props.idName}></div>
     )
+  }
+}
+
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  //Add the basic timer functionality
+  //Add the switch timer functionality
+  //Add the noise switch timer functionality
+
+  //Add the play functionality
+  //Add the pause functionality
+  //Add the stop functionality
+  render() {
+    return (
+      <React.Fragment>
+        <div>Timer</div>
+        <span>Play</span>
+        <span>Pause</span>
+        <span>Stop</span>
+       </React.Fragment>
+    );
   }
 }
 
